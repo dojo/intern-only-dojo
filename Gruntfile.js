604febbc7b7927080ae65b4f3cd89cf854dcca6f
Bryan Forbes <bryan@reigndropsfall.net>
2013-11-18 13:57:29 -0600

    Gruntfile fixes
    * Work around ts:dev failing before watch task runs
    * Ignore interfaces.ts for compilation
    * Mark all files for recompile if interfaces.ts changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		ts: {
 			dev: {
-				src: [ '**/*.ts', '!node_modules/**/*.ts' ],
+				src: [ '**/*.ts', '!node_modules/**/*.ts', '!interfaces.ts' ],
 				outDir: '.',
 				options: {
 					target: 'es5',
@@ -31,12 +31,28 @@ module.exports = function (grunt) {
 		}
 	});
 
-	grunt.registerTask('default', [ 'ts:dev', 'watch:ts' ]);
+	var previousForceState = grunt.option('force');
+
+	grunt.registerTask('force', function (set) {
+		if (set === 'on') {
+			grunt.option('force', true);
+		}
+		else if (set === 'off') {
+			grunt.option('force', false);
+		}
+		else if (set === 'restore') {
+			grunt.option('force', previousForceState);
+		}
+	});
+
+	grunt.registerTask('default', [ 'force:on', 'ts:dev', 'force:restore', 'watch:ts' ]);
 	grunt.registerTask('build', [ 'ts:dev' ]);
 
 	var changedFiles = {},
-		onChange = grunt.util._.debounce(function () {
-			grunt.config('ts.dev.src', Object.keys(changedFiles));
+		onChange = grunt.util._.debounce(function (all) {
+			if (!changedFiles['interfaces.ts']) {
+				grunt.config('ts.dev.src', Object.keys(changedFiles));
+			}
 			changedFiles = {};
 		}, 200);
 	grunt.event.on('watch', function (action, filepath) {
