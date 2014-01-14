a30bb9c8aa229ec0a6517f6857d69950b379451c
Bryan Forbes <bryan@reigndropsfall.net>
2014-01-14 17:58:13 -0600

    Order declarations alphabetically (case-insensitive)

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -1,25 +1,28 @@
-export interface Require {
-	(config:any):void;
-	(config:any, modules:string[]):void;
-	(config:any, modules:string[], factory:Function):void;
-	(modules:string[]):void;
-	(modules:string[], factory:Function):void;
-	(moduleName:string):any;
-	has: {
-		(feature:string):any;
-		add(feature:string, test:any, now?:boolean, force?:boolean):void;
-	}
-	toUrl: (moduleId:string)=>string;
-	nodeRequire: (moduleId:string)=>any;
-}
-export declare var require: Require;
-
 export interface Define {
 	(factory:Function):void;
 	(dependencies:string[], factory:Function):void;
 	(value:any):void;
 }
 export declare var define: Define;
+
+export interface IEvented {
+	on(type:string, listener:Function):IHandle;
+	emit(type:string, ...args:any[]):boolean;
+}
+
+export interface IExtensionEvent {
+	(target:any, listener:Function):IHandle;
+}
+
+export interface IHandle {
+	remove():void;
+}
+
+export interface IHas extends ILoaderFunctionPlugin {
+	(feature:string):any;
+	add(feature:string, test:(global:any, document:Document, element:HTMLElement)=>boolean, now?:boolean, force?:boolean):void;
+	add(feature:string, test:boolean, now?:boolean, force?:boolean):void;
+}
 
 export interface ILoaderPlugin {
 	normalize?(id:string, normalize:Function):string;
@@ -30,24 +33,6 @@ export interface ILoaderFunctionPlugin extends ILoaderPlugin {
 	(...args:any[]):any;
 }
 
-export interface IHas extends ILoaderFunctionPlugin {
-	(feature:string):any;
-	add(feature:string, test:(global:any, document:Document, element:HTMLElement)=>boolean, now?:boolean, force?:boolean):void;
-	add(feature:string, test:boolean, now?:boolean, force?:boolean):void;
-}
-
-export interface IHandle {
-	remove():void;
-}
-
-export interface IEvented {
-	on(type:string, listener:Function):IHandle;
-	emit(type:string, ...args:any[]):boolean;
-}
-
-export interface IExtensionEvent {
-	(target:any, listener:Function):IHandle;
-}
 export interface IOn {
 	(target:HTMLElement, type:string, listener:Function, capture?:boolean):IHandle;
 	(target:HTMLElement, type:IExtensionEvent, listener:Function, capture?:boolean):IHandle;
@@ -65,6 +50,7 @@ export interface IPromiseFunction<T> {
 	(value:T):void;
 	(promise:IPromise<T>):void;
 }
+
 export interface IPromiseResolver<T> {
 	(resolve:IPromiseFunction<T>, reject:IPromiseFunction<any>):void;
 }
@@ -77,6 +63,7 @@ export interface IPromise<T> {
 	then<U>(onFulfilled?:(value:T)=>IPromise<U>, onRejected?:(reason:any)=>U):IPromise<U>;
 	then<U>(onFulfilled?:(value:T)=>IPromise<U>, onRejected?:(reason:any)=>IPromise<U>):IPromise<U>;
 }
+
 export declare var IPromise: {
 	new <T>(resolver:IPromiseResolver<T>);
 };
@@ -84,3 +71,19 @@ export declare var IPromise: {
 export interface IRegistryMatcher {
 	(...args:any[]):boolean;
 }
+
+export interface Require {
+	(config:any):void;
+	(config:any, modules:string[]):void;
+	(config:any, modules:string[], factory:Function):void;
+	(modules:string[]):void;
+	(modules:string[], factory:Function):void;
+	(moduleName:string):any;
+	has: {
+		(feature:string):any;
+		add(feature:string, test:any, now?:boolean, force?:boolean):void;
+	}
+	toUrl: (moduleId:string)=>string;
+	nodeRequire: (moduleId:string)=>any;
+}
+export declare var require: Require;
