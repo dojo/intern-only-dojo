6ea13d96dc08f822fda41fabb1ac43cd6bbc8350
Kitson Kelly <dojo@kitsonkelly.com>
2014-12-07 08:55:57 +0000

    Updating package.json and tests/config.

--- tests/config.ts
+++ tests/config.ts
@@ -12,7 +12,7 @@ var internConfig = {
 	// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
 	// automatically
 	capabilities: {
-		'selenium-version': '2.37.0'
+		'selenium-version': '2.41.0'
 	},
 
 	// Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
@@ -22,8 +22,8 @@ var internConfig = {
 		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
 		{ browserName: 'internet explorer', version: [ '9', '10' ], platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '25', platform: [ 'OS X 10.6', 'Windows 7', 'Windows XP', 'Linux' ] },
-		{ browserName: 'chrome', version: '', platform: [ 'Linux', 'OS X 10.8', 'OS X 10.9', 'Windows XP', 'Windows 7', 'Windows 8', 'Windows 8.1' ] },
+		{ browserName: 'firefox', version: '28', platform: [ 'OS X 10.9', 'Windows 7', 'Windows XP', 'Linux' ] },
+		{ browserName: 'chrome', version: '', platform: [ 'Linux', 'OS X 10.9', 'Windows XP', 'Windows 7', 'Windows 8', 'Windows 8.1' ] },
 		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' }/*,
 		{ browserName: 'safari', version: '7', platform: 'OS X 10.9' }*/
 	],
@@ -31,29 +31,28 @@ var internConfig = {
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
 	maxConcurrency: 3,
 
-	// Whether or not to start Sauce Connect before running tests
-	useSauceConnect: true,
+	// Name of the tunnel class to use for WebDriver tests
+	tunnel: 'SauceLabsTunnel',
 
-	// Connection information for the remote WebDriver service. If using Sauce Labs, keep your username and password
-	// in the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables unless you are sure you will NEVER be
-	// publishing this configuration file somewhere
-	webdriver: {
-		host: 'localhost',
-		port: 4444
+	// The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
+	// loader
+	useLoader: {
+		'host-node': 'dojo/dojo',
+		'host-browser': 'node_modules/dojo/dojo.js'
 	},
 
 	// Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
 	// can be used here
 	loader: {
 		// Packages that should be registered with the loader in each testing environment
-		packages: [ { name: 'core', location: '.' } ]
+		packages: [ { name: 'dojo2', location: '.' } ]
 	},
 
 	// Non-functional test suite(s) to run in each browser
-	suites: [ 'core/tests/unit/all' ],
+	suites: [ 'dojo2/tests/unit/all' ],
 
 	// Functional test suite(s) to run in each browser once non-functional tests are completed
-	functionalSuites: [ 'core/tests/functional/all' ],
+	functionalSuites: [ 'dojo2/tests/functional/all' ],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
 	excludeInstrumentation: /^(?:node_modules|tests)\//
