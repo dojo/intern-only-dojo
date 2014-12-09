f86750ed2925cefbcc029b9b201945f1177790d3
Bryan Forbes <bryan@reigndropsfall.net>
2014-12-09 16:38:37 -0600

    Make the prepublish script use the `tsc` in node_modules

--- support/prepublish.sh
+++ support/prepublish.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find . -name '*.ts' -not -wholename '*/tests/*' -not -wholename '*/node_modules/*' -not -name '*.d.ts' -exec tsc -m commonjs -t ES5 {} +
+find . -name '*.ts' -not -wholename '*/tests/*' -not -wholename '*/node_modules/*' -not -name '*.d.ts' -exec ./node_modules/.bin/tsc -m commonjs -t ES5 {} +
